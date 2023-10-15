3c91dc55a91268deb1f7e76fa6a9b2de05ab80ad
teshanj7 <teshanjayakody217@gmail.com>
2023-10-15 11:20:23 +0530

    mobile changes

--- backend/models/material.js
+++ backend/models/material.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 const materialSchema = new mongoose.Schema({
-  MaterialID: {
-    type: String,
-    required: true
-  },
+  // MaterialID: {
+  //   type: String,
+  //   required: true
+  // },
   MaterialName: {
     type: String,
     required: true
   },
-  MaterialPrice: {
-    type: Number,
-    required: true
-  },
+  // MaterialPrice: {
+  //   type: Number,
+  //   required: true
+  // },
   MaterialQuantity: {
     type: Number,
     required: true
