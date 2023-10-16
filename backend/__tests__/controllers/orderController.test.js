944c05517df3da8907b0887b3113f904df7b61ed
Keshan <janindupathi@gmail.com>
2023-10-16 21:36:37 +0530

    Success test cases

--- backend/__tests__/controllers/orderController.test.js
+++ backend/__tests__/controllers/orderController.test.js
@@ -1,66 +1,48 @@
 // Import necessary modules and dependencies
 const Order = require('../../models/order');
 const orderController = require('../../controllers/orderController');
-const request = require('supertest');
-const app = require('../../server');
 
-// Import your MongoDB connection or configuration
-const mongoose = require('mongoose');
-const { mongoURI } = require('../../config/database');
+// Mock Order.save to return data in orderController
+jest.mock('../../models/order');
 
-// Mock Order.find to return data in orderController
-jest.mock('../../models/order', () => ({
-    find: jest.fn(),
-}));
-
-
-const dbUri = 'mongodb+srv://kesh:1234@cluster0.wrq1qps.mongodb.net/Construction_Database?retryWrites=true&w=majority';
-beforeAll(async () => {
-    await mongoose.connect(dbUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-});
-
-// Test cases for allOrders function
+// Test cases for createOrder function
 describe('orderController', () => {
-    describe('allOrders', () => {
-        it('should get all orders', async () => {
-            // Mock the behavior of Order.find to return data
-            const mockData = [
-                {
-                    _id: '652b765a7cbba9f6dd7465d2',
-                    SupplierName: 'Sahan',
-                    RequisitionID: '652ace0ba341e939c0d8feac',
-                    SiteManagerID: 'SM001',
-                    SiteManagerName: 'Saman',
-                    SiteName: 'Kandy site',
-                    Date: '2023-11-15',
-                    ToatalAmount: '25000',
-                    Materials: [{
-                        MaterialName: 'Material B',
-                        MaterialQuantity: 3
-                    }],
-                    CardType: 'debit',
-                    CardNumber: 212423424524
-                },
-                // Add more order data as needed
-            ];
+    describe('createOrder', () => {
 
-            // Set up the mock for Employee.find
-            Order.find.mockResolvedValue(mockData);
+        const request = {
+            body: {
+                "SupplierName": "Sahan",
+                "RequisitionID": "652ace0ba341e939c0d8feac",
+                "SiteManagerID": "SM001",
+                "SiteManagerName": "Saman",
+                "SiteName": "Kandy site",
+                "Date": "2023-11-15",
+                "TotalAmount": "25000", // Fixed the property name to "TotalAmount"
+                "Materials": [
+                    {
+                        "MaterialName": "Material B",
+                        "MaterialQuantity": 3
+                    }
+                ],
+                "CardType": "debit",
+                "CardNumber": 212423424524
+            }
+        };
 
-            // Use supertest to make a request to your route
-            const response = await request(app).get('/orders/allOrders');
+        const response = {
+            json: jest.fn((x) => x)
+        };
 
-            // Assertions
-            expect(response.status).toBe(200);
-            expect(response.body).toEqual(mockData);
+        it('should create an order', async () => {
+            const mockSave = jest.fn().mockResolvedValue({}); // Mock the save function
+            Order.mockImplementationOnce(() => ({
+                save: mockSave,
+            }));
+
+            await orderController.createOrder(request, response);
+
+            expect(mockSave).toHaveBeenCalled();
+            expect(response.json).toHaveBeenCalledWith("Order Added");
         });
     });
 });
-
-afterAll(async () => {
-    await mongoose.connection.close(); // Close the MongoDB connection
-});
-
